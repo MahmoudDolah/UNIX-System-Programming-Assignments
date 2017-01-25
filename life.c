125c7cdd554a75294604e906fbd5c7c4daaf3b9f
Mahmoud <md2989@nyu.edu>
2017-01-25 16:23:31 -0500

    Added functionality to take in command line arguments

--- life.c
+++ life.c
@@ -1,6 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main()
+int main(int argc, char**argv)
 {
-    printf("The Game of Life");
+    printf("The Game of Life\n");
+    // Default values for command line arguments
+    int rows = 10;
+    int columns = 10;
+    int generations = 10;
+    char* filename = "life.txt";
+    char *ptr;
+
+    for (int i = 1; i < argc; i++){
+        if (i == 1){
+            rows = strtol(argv[i], &ptr, 10);
+        }
+        else if (i == 2){
+            columns = strtol(argv[i], &ptr, 10);
+        }
+        else if (i == 3){
+            filename = argv[i];
+        }
+        else if (i == 4){
+          generations = strtol(argv[i], &ptr, 10);
+        }
+    }
+/*
+    printf("Rows: %d\n", rows);
+    printf("Columns: %d\n", columns);
+    printf("filename: %s\n", filename);
+    printf("Generations: %d\n", generations);
+*/
 }
