c576ab9f098e215e5998d23e160d2c6a56ba1afc
Mahmoud <md2989@nyu.edu>
2017-02-01 12:34:25 -0500

    Implemented command line arguments, and game mechanics

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Game-Of-Life
-Assignment from my UNIX System Programming class
+Unfortunately, my implementation does not work.
+
+## My Approach            
+First, I created two structs, one for an individual cell (called cell), and another for the game board (called world). The cell struct has two members, neighbors and alive. Obviously, alive is whether the is alive or not. The neighbors field is a pointer to an array of the surrounding cells. The idea was that it would be easier to update the board if you have direct access to the nearby cells.           
+Now, the world struct has a pointer to the cells (called positions), the number of rows, number of columns, and, most importantly, a pointer to the memory. A big reason why I chose to structure my code this way was to avoid memory leaks. My deleteWorld function only has to free up the memory that is already being tracked by the board itself.                          
+I thought this would be a good idea because I have used C before when I took an Intro to C class back in high school and did something similar.
+I think a big problem with my attempt is that it is a classic case of over engineering. After speaking with a few classmates, all of their examples simply made the game board a matrix (an array containing an array) and if that field contained a " * " character, then it meant that the cell was alive.
+
+
+## What Went Wrong and Other Approaches
+It causes a segmentation fault in the initializeWorld function and, despite my attempts, I am unable to find the mistake. However, I do believe that I confused rows, columns, and the line length at some point. I have worked my way backwards with gdb, but am unable to find the solution.
+During my attempts to use gdb, it kept claiming that there was a problem with "iofgets.c", which leads to believe that there is an error associated with the way I used the fgets function. However, I have been unable solve this (although, perhaps I could have followed Linus's Law to get a solution).              
+For completeness, I have written the entire program for you to investigate.
+
+
+## What I Learned
+KISS - Keep It Simple, Stupid.        
+To be honest, I believe that I made my solution too complex, especially since this is the first time I have touched C (or C++) in well over a year and a half (since I took OS in fall of 2015).
+
+## How to Run the Code
+You can run the code by entering the directory and typing
+```
+make
+```
+This will create an executable called ```life```.
+Now, enter
+```
+./life <rows> <columns> <filename> <generations>
+```
+to run the program.
--- life.c
+++ life.c
@@ -2,15 +2,31 @@
 #include<stdlib.h>
 #include<string.h>
 
-int main(int argc, char**argv)
-{
-    printf("The Game of Life\n");
-    // Default values for command line arguments
+typedef struct cell{
+    struct cell* neighbors[8]; // The 8 cells that surround it
+    int alive;
+} cell;
+
+typedef struct world{
+    cell** positions;
+    int rows;
+    int columns;
+    void* mem;
+} world;
+
+void createWorld(world* gameoflife, int rows, int columns);
+void initializeWorld(const char* filename, world* gameoflife, int rows, int columns);
+void updateWorld(world* gameoflife);
+void printWorld(world* gameoflife);
+void destroyWorld(world* gameoflife);
+
+int main (int argc, char** argv){
     int rows = 10;
     int columns = 10;
     int generations = 10;
-    char* filename = "life.txt";
+    char *filename = "life.txt";
     char *ptr;
+    world gameoflife;
 
     for (int i = 1; i < argc; i++){
         if (i == 1){
@@ -23,13 +39,134 @@ int main(int argc, char**argv)
             filename = argv[i];
         }
         else if (i == 4){
-          generations = strtol(argv[i], &ptr, 10);
+            generations = strtol(argv[i], &ptr, 10);
         }
     }
-/*
-    printf("Rows: %d\n", rows);
-    printf("Columns: %d\n", columns);
-    printf("filename: %s\n", filename);
-    printf("Generations: %d\n", generations);
-*/
+
+    createWorld(&gameoflife, rows, columns);
+    initializeWorld(filename, &gameoflife, rows, columns);
+    for (int i = 0; i < generations; i++){
+        updateWorld(&gameoflife);
+        printWorld(&gameoflife);
+    }
+    destroyWorld(&gameoflife);
+}
+void destroyWorld(world* gameoflife){
+    free(gameoflife->mem);
+}
+
+void printWorld(world* gameoflife){
+    for (int i = 0; i < gameoflife->columns; i++){
+        for (int j = 0; j < gameoflife->rows; j++){
+            if (gameoflife->positions[i][j].alive == 1){
+                printf("*");
+            }
+            else if (gameoflife->positions[i][j].alive == 0){
+                printf(" ");
+            }
+        }
+    }
+}
+
+void updateWorld(world* gameoflife){
+    int row, column, counter, neighbors;
+    // row and column are positions
+    // counter is a counter for the neighbors next to a cell
+
+    for (row = 0; row < gameoflife->rows; row++){
+        for (column = 0; column < gameoflife->columns; column++){
+            for (counter = 0; counter < 8; counter++){
+                if ((gameoflife->positions[row][column].neighbors[counter]) && ((gameoflife->positions[row][column]).neighbors[counter]->alive == 1)){
+                    neighbors++;
+                }
+            }
+            if (gameoflife->positions[row][column].alive == 1){
+                if (neighbors < 2){
+                    gameoflife->positions[row][column].alive = 0;
+                }
+                else if(neighbors == 3){
+                    gameoflife->positions[row][column].alive = 0;
+                }
+            }
+            else if (gameoflife->positions[row][column].alive == 0){
+                if (neighbors == 3){
+                    gameoflife->positions[row][column].alive = 1;
+                }
+            }
+        }
+    }
+
+}
+
+void initializeWorld(const char* filename, world* gameoflife, int rows, int columns){
+    /* Takes input from file and puts it into the gameoflife array
+    */
+    FILE* fp;
+    fp = fopen(filename, "r");
+    char* read = NULL;
+
+    for (int i = 0; i < columns; i++){
+        char * line = fgets(read, rows, fp);
+        for (int j = 0; j < rows; j++){
+            if (strcmp(&line[j], "\n")){
+                break;
+            }
+            else if (strcmp(&line[j], "*")){
+                gameoflife->positions[i][j].alive = 1;
+            }
+        }
+    }
+    fclose(fp);
+}
+
+void createWorld(world* gameoflife, int rows, int columns){
+    int i, j; // i and j are the coordinates on the board
+    unsigned long length = sizeof(cell *) * columns;
+    unsigned long height = sizeof(cell) * rows;
+
+    gameoflife->mem = malloc(length + (height * columns));
+
+    gameoflife->positions  = gameoflife->mem;
+    gameoflife->rows  = rows;
+    gameoflife->columns = columns;
+
+    for(i = 0; i < columns; i++) {
+       gameoflife->positions[i] = gameoflife->mem + length + (i * height);
+    }
+
+    for(i = 0; i < columns; i++) {
+        for(j = 0; j < rows; j++) {
+            if(j != 0) {
+                (gameoflife->positions[i][j]).neighbors[3] = &(gameoflife->positions[i][j - 1]);
+            }
+
+            if(i != 0) {
+                (gameoflife->positions[i][j]).neighbors[1] = &(gameoflife->positions[i - 1][j]);
+            }
+
+            if(j != (rows - 1)) {
+                (gameoflife->positions[i][j]).neighbors[4] = &(gameoflife->positions[i][j + 1]);
+            }
+
+            if(i != (columns - 1)) {
+                (gameoflife->positions[i][j]).neighbors[6] = &(gameoflife->positions[i + 1][j]);
+            }
+
+            if((i != 0) && (j != 0)) {
+                (gameoflife->positions[i][j]).neighbors[0] = &(gameoflife->positions[i - 1][j - 1]);
+            }
+
+            if((i != (columns - 1)) && (j != (rows - 1))) {
+                (gameoflife->positions[i][j]).neighbors[7] = &(gameoflife->positions[i + 1][j + 1]);
+            }
+
+            if((i != (columns - 1)) && (j != 0)) {
+                (gameoflife->positions[i][j]).neighbors[5] = &(gameoflife->positions[i + 1][j - 1]);
+            }
+
+            if((i != 0) && (j != (rows - 1))) {
+                (gameoflife->positions[i][j]).neighbors[2] = &(gameoflife->positions[i - 1][j + 1]);
+            }
+        }
+    }
 }
