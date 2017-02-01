1745323e3328440035511f0f6aac061f9c5e50ff
Mahmoud <md2989@nyu.edu>
2017-02-01 16:12:08 -0500

    Handles command line arguments and display function

--- hw2-process-environment/env.c
+++ hw2-process-environment/env.c
@@ -2,6 +2,37 @@
 #include<stdlib.h>
 #include<string.h>
 
+extern char** environ;
+
+void display();
+
 int main (int argc, char** argv){
-    
+    char* nameVal;
+    char* cmd;
+    char* arg;
+    if (argc > 1){
+        for (int i = 1; i < argc; i++){
+            if (i == 2){
+                nameVal = argv[i];
+            }
+            else if (i == 3){
+                cmd = argv[i];
+            }
+            else if (i == 4){
+                arg = argv[i];
+            }
+        }
+    }
+    display();
+}
+
+void display(){
+    for (char* c = *environ; c; c=*++environ){
+        puts(c);
+    }
+    /*
+    for (char** p = environ; *p; p++){
+        puts(*p);
+    }
+    */
 }
