d3a1a7f3ba9275df8fe03336d1597a8c171ac73d
Mahmoud <md2989@nyu.edu>
2017-02-07 15:45:25 -0500

    Handles all command line inputs

--- hw2-process-environment/env.c
+++ hw2-process-environment/env.c
@@ -6,33 +6,74 @@ extern char** environ;
 
 void display();
 
-int main (int argc, char** argv){
-    char* nameVal;
-    char* cmd;
-    char* arg;
-    if (argc > 1){
-        for (int i = 1; i < argc; i++){
-            if (i == 2){
-                nameVal = argv[i];
-            }
-            else if (i == 3){
-                cmd = argv[i];
-            }
-            else if (i == 4){
-                arg = argv[i];
+int main (int argc, char* argv[]){
+    //char* nameVal;
+    //char* cmd[5];
+    //char* arg[5];
+    int startNameVal = 0;
+    int endNameVal = 0;
+    int iFlag = 0;
+    int isCommand = 0;
+    int enter = 0;
+    for (int i = 1; i < argc; i++){
+        if (strcmp(argv[i-1], "-i") == 0){
+            iFlag = 1;
+        }
+        if ((iFlag == 1) & (enter == 0)){
+            startNameVal = i;
+            enter = 1;
+        }
+        if (iFlag == 1){
+            endNameVal = i;
+            if (strpbrk(argv[endNameVal], "=") != 0){
+                //argv[j];
+                endNameVal++; // Count total number of commands
+                // Causes seg fault bc checks over edge
             }
         }
     }
-    display();
+    char* nameval[(endNameVal)-startNameVal];
+    int j = 0;
+    for (int i = startNameVal; i < endNameVal-1; i++){
+        nameval[j] = argv[i];
+        j++;
+    }
+    char* commands[argc - (endNameVal-1)];
+    int k = 0;
+    for (int i = (endNameVal-1); i < argc; i++){
+        commands[k] = argv[i];
+        k++;
+    }
+    if (k > 0){
+        isCommand = 1;
+    }
+    /*
+    printf("startNameVal: %d, endNameVal: %d\n", startNameVal, endNameVal);
+    printf("All of argv\n");
+    for (int i = 0; i < argc; i++){
+        printf("%s\n", argv[i]);
+    }
+    printf("Just path part of argv\n");
+    for (int i = (startNameVal-1); i < endNameVal; i++){
+        printf("%s\n", argv[i]);
+    }
+    printf("nameval array\n");
+    for (int i = 0; i < (endNameVal - startNameVal); i++){
+        printf("%s\n", nameval[i]);
+    }
+    printf("commands array\n");
+    for (int i = 0; i < k; i++){
+        printf("%s\n", commands[i]);
+    }
+    */
+    if ((iFlag == 0) & (isCommand == 0)){
+        display();
+    }
+
 }
 
 void display(){
-    for (char* c = *environ; c; c=*++environ){
+    for (char* c = *environ; c; c = *++environ){
         puts(c);
     }
-    /*
-    for (char** p = environ; *p; p++){
-        puts(*p);
-    }
-    */
 }
